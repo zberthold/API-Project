d3976b3abd27c394a9e78dbb7fb6efc0f4e181d0
Zoe Berthold <zoeberthold@zoes-macbook-pro.local>
2018-07-11 11:48:30 -0500

    parse json data

--- API Project/CurrenciesViewController.swift
+++ API Project/CurrenciesViewController.swift
@@ -11,18 +11,46 @@ import UIKit
 class CurrenciesViewController: UITableViewController {
 
     var currencies = [[String: String]]()
+    var typesOfCurrencies = ["USD", "GBP", "EUR"]
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Currencies"
         let query = "https://api.coindesk.com/v1/bpi/currentprice.json"
-        // Do any additional setup after loading the view, typically from a nib.
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                parse(json: json)
+                return
+            }
+        }
+        loadError()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func parse (json: JSON){
+//        var cool = json["bpi"]
+//        var moreCool = cool.dictionaryValue
+//        for currency in typesOfCurrencies {
+//            var value = moreCool[currency]
+//            print(value!)
+//        }
+        
+        for result in typesOfCurrencies{
+            var cool = json["bpi"][result]["code"]
+            print(cool)
+        }
+        tableView.reloadData()
+    }
+    func loadError() {
+        let alert = UIAlertController(title: "Loading Error",
+                                      message: "There was a problem loading the news feed",
+                                      preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
